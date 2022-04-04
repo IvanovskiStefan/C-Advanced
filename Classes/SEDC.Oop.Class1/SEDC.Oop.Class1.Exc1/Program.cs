9c0c9265c87a0a87596619dff857af547873b453
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-04-04 11:43:02 -0700

    UpdateExcerciseFromClass1

--- Classes/SEDC.Oop.Class1/SEDC.Oop.Class1.Exc1/Program.cs
+++ Classes/SEDC.Oop.Class1/SEDC.Oop.Class1.Exc1/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//exc 1
+
+Console.WriteLine("please enter  names");
+Console.WriteLine("If you want to exit the app please entert X");
+string inputFromConsole = Console.ReadLine();
+List<string> Names = new List<string>();
+
+while (inputFromConsole != "x")
+{
+    Names.Add(inputFromConsole);
+    inputFromConsole = Console.ReadLine();
+    
+}
+
+Console.WriteLine("now enter some sentence and check how many times the name appears");
+string sentenceInput = Console.ReadLine();
+
+      
+
+string [] sentenceToWords = sentenceInput.Split(" ");
+
+
+foreach (string Name in Names)
+{
+    int counter = 0;
+    foreach (string word in sentenceToWords)
+    {
+
+
+        if (word == Name)
+        {
+            counter++;
+            
+        }
+    }
+
+    Console.WriteLine($"Name {Name} : {counter}");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Classes/SEDC.Oop.Class1/SEDC.Oop.Class1.Exc3/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+//2.Create a console application that checks if a day is a working day 🔹
+//The app should request for a user to enter a date as an input or multiple inputs
+//The app should then open and see if the day is a working day
+//It should show the user a message whether the date they entered is working or not
+//-Weekends are not working
+//-1 January, 7 January, 20 April, 1 May, 25 May, 3 August, 8 September, 12 October, 23 October, and 8 December are not working days as well
+//It should ask the user if they want to check another date
+//Yes - the app runs again
+//No - the app closes
+
+List<DateTime> listOfDays = new List<DateTime>
+{
+    new DateTime(2000,1,1),
+    new DateTime(2000,7,1),
+    new DateTime(2000,5,1),
+    new DateTime(2000,5,25),
+    new DateTime(2000,8,3),
+    new DateTime(2000,5,1),
+    new DateTime(2000,8,3),
+    new DateTime(2000,9,8),
+    new DateTime(2000,10,12),
+    new DateTime(2000,10,23),
+    new DateTime(2000,12,8)
+
+};
+
+Console.WriteLine("Please enter the date that you want to check");
+string inputFromConsole = Console.ReadLine();
+bool isValid = DateTime.TryParse(inputFromConsole, out DateTime parsedDate);
+
+
+bool isHoliday = false;
+
+foreach (DateTime date in listOfDays)
+{
+    if (date.Month == parsedDate.Month && date.Day == parsedDate.Day)
+    {
+        isHoliday = true;
+        break;
+    }
+    
+}
+
+
+
+
+if (parsedDate.DayOfWeek == DayOfWeek.Sunday || parsedDate.DayOfWeek == DayOfWeek.Saturday)
+{
+    Console.WriteLine("Weekeeennnddd !!!!");
+}
+
+ else if (isHoliday)
+{
+    Console.WriteLine("IS HOLIDAY !!");
+}
+
+
+else
+{
+
+    Console.WriteLine("WORKING TODAAYY !");
+}
