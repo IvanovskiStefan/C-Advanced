29f39d3daa578d1dc4159273cd17133cd4dec3fd
Stefan Ivanovski <ivanovski.st@gmail.com>
2022-04-11 20:27:18 +0200

    Class04

---
+++ Classes/SEDC.Oop.Class4/SEDC.Oop.Class4.Extensions/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using SEDC.Oop.Class4.Extensions.Extensions;
+
+Console.WriteLine(StringExtensions.DeleteLastCharacterClassic("String to shorten"));
+
+string textToShorten = "Some string value";
+
+Console.WriteLine(textToShorten.DeleteLastCharacter());
+
+string textToQuote = "Quoted Text";
+
+Console.WriteLine(textToQuote.AddQuotations());
+
+List<string> list = new List<string>() { "First", "Second", "Third", "Fourth", "Fifth" };
+
+
+list.PrintElements();
